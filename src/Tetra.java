f616b98436262c20767cd0969c502de1521d796d
Thomas Ersfeld <thomas.ersfeld@gmail.com>
2015-09-17 16:33:14 +0200

    taiste

--- src/Tetra.java
+++ src/Tetra.java
@@ -5,16 +5,44 @@ import org.paukov.combinatorics.ICombinatoricsVector;
 
 public class Tetra {
 
-    public static void main (String[] args)
-    {
-        // Create the initial vector of 3 elements (apple, orange, cherry)
-        ICombinatoricsVector<String> originalVector = Factory.createVector(new String[]{"A", "C", "G", "T"});
+    public static void main (String[] args) {
+        ICombinatoricsVector<String> allTetra = Factory.createVector(new String[]{"A", "C", "G", "T"});
+        ICombinatoricsVector<String> allTetraCompl = Factory.createVector(new String[]{"T", "G", "C", "A"});
 
         // Create the permutation generator by calling the appropriate method in the Factory class
-        Generator<String> gen = Factory.createPermutationWithRepetitionGenerator(originalVector, 4);
+        Generator<String> gen = Factory.createPermutationWithRepetitionGenerator(allTetra, 4);
 
         // Print the result
         for (ICombinatoricsVector<String> perm : gen)
             System.out.println(perm);
+
+        String tetra1 = "ACGT";
+        String tetra2 = "CGCT";
+
+        System.out.println("tetra: " + tetra1 + "\tcompl:" + Compl(tetra1));
+        System.out.println("tetra: " + tetra2 + "\tcompl:" + Compl(tetra2));
+
+    }
+
+    public static String Compl(String tetra)
+    {
+        StringBuilder res = new StringBuilder();
+
+        for (char ch: tetra.toCharArray()) {
+            char chCompl = 'A';
+
+            if (ch == 'A')
+                chCompl ='T';
+            if (ch == 'C')
+                chCompl ='G';
+            if (ch == 'G')
+                chCompl ='C';
+            if (ch == 'T')
+                chCompl ='A';
+
+            res.insert(0,chCompl);
+        }
+
+        return res.toString();
     }
 }
