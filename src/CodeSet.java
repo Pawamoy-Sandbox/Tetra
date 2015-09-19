a9e56c51a80cb672cc3f4b402e129fc5140fda3b
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2015-09-19 12:30:24 +0200

    Add BS16

--- src/CodeSet.java
+++ src/CodeSet.java
@@ -12,6 +12,7 @@ public class CodeSet {
     public static List<String> S12 = new ArrayList<>();
     public static List<String> S16 = new ArrayList<>();
     public static BitSet BS12 = new BitSet();
+    public static BitSet BS16 = new BitSet();
     public static List<Integer> SByteCompl = new ArrayList<>();
 
     // Singleton Stuff
@@ -75,6 +76,7 @@ public class CodeSet {
                     continue;
 
                 S16.add(line);
+                BS16.set(i);
 
                 i++;
             }
